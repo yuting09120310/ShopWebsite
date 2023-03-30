d71b0d9ae65730445e4f0813d7c8eafd7c3b1686
alex <yuting09120310@hotmail.com>
2023-03-31 01:07:15 +0800

    補上model 顯示名稱

---
+++ AlexBlogMVC/BackEnd/Controllers/ProductController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AlexBlogMVC.BackEnd.Models;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace AlexBlogMVC.BackEnd.Controllers
+{
+    public class ProductController : GenericController
+    {
+
+        public ProductController(BlogMvcContext context) : base(context) { }
+
+
+        //當每個action被執行都會呼叫getMenu
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            getMenu();
+            base.OnActionExecuting(context);
+        }
+
+
+        // GET: Product
+        public async Task<IActionResult> Index()
+        {
+              return _context.Products != null ? 
+                          View(await _context.Products.ToListAsync()) :
+                          Problem("Entity set 'BlogMvcContext.Products'  is null.");
+        }
+
+        // GET: Product/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null || _context.Products == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .FirstOrDefaultAsync(m => m.ProductNum == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // GET: Product/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ProductNum,Lang,ProductClass,ProductSort,ProductDepartment,ProductId,ProductTitle,ProductDescription,ProductContxt,ProductImg1,ProductImgUrl,ProductImgAlt,ProductImgList,ProductImgListAlt,ProductVideo1,ProductPublish,ProductPutTime,CreateTime,Creator,EditTime,Editor,Ip,ProductOffTime")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+        // GET: Product/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null || _context.Products == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Product/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("ProductNum,Lang,ProductClass,ProductSort,ProductDepartment,ProductId,ProductTitle,ProductDescription,ProductContxt,ProductImg1,ProductImgUrl,ProductImgAlt,ProductImgList,ProductImgListAlt,ProductVideo1,ProductPublish,ProductPutTime,CreateTime,Creator,EditTime,Editor,Ip,ProductOffTime")] Product product)
+        {
+            if (id != product.ProductNum)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(product);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductExists(product.ProductNum))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+        // GET: Product/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null || _context.Products == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .FirstOrDefaultAsync(m => m.ProductNum == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            if (_context.Products == null)
+            {
+                return Problem("Entity set 'BlogMvcContext.Products'  is null.");
+            }
+            var product = await _context.Products.FindAsync(id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductExists(long id)
+        {
+          return (_context.Products?.Any(e => e.ProductNum == id)).GetValueOrDefault();
+        }
+    }
+}
--- AlexBlogMVC/BackEnd/Models/News.cs
+++ AlexBlogMVC/BackEnd/Models/News.cs
@@ -1,45 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace AlexBlogMVC.BackEnd.Models;
 
 public partial class News
 {
+    [Display(Name = "新聞編號")]
     public long NewsNum { get; set; }
 
     public string? Lang { get; set; }
 
+    [Display(Name = "新聞類別")]
     public int? NewsClass { get; set; }
 
     public int? NewsSort { get; set; }
 
+    [Display(Name = "標題")]
     public string? NewsTitle { get; set; }
 
+    [Display(Name = "簡述")]
     public string? NewsDescription { get; set; }
 
+    [Display(Name = "內文")]
     public string? NewsContxt { get; set; }
 
+    [Display(Name = "圖片")]
     public string? NewsImg1 { get; set; }
 
+    [Display(Name = "連結")]
     public string? NewsImgUrl { get; set; }
 
     public string? NewsImgAlt { get; set; }
 
+    [Display(Name = "狀態")]
     public int? NewsPublish { get; set; }
 
     public int? NewsViews { get; set; }
 
+    [Display(Name = "上架時間")]
     public DateTime? NewsPutTime { get; set; }
 
+    [Display(Name = "建立時間")]
     public DateTime? CreateTime { get; set; }
 
+    [Display(Name = "建立人")]
     public int? Creator { get; set; }
 
+    [Display(Name = "編輯時間")]
     public DateTime? EditTime { get; set; }
 
+    [Display(Name = "編輯人")]
     public int? Editor { get; set; }
 
     public string? Ip { get; set; }
 
+    [Display(Name = "下架時間")]
     public DateTime? NewsOffTime { get; set; }
 }
