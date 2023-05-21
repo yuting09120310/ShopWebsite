8677a5f6f46f2769390078bcbe0ac8fb74485c6c
yuting <yuting09120310@gmail.com>
2023-05-22 02:58:00 +0800

    前台banner完成

--- AlexBlogMVC/FrontEnd/Controllers/GenericController.cs
+++ AlexBlogMVC/FrontEnd/Controllers/GenericController.cs
@@ -15,66 +15,11 @@ namespace AlexBlogMVC.FrontEnd.Controllers
             _context = context;
         }
 
-        // 呼叫每一個action都會執行
-        public override void OnActionExecuting(ActionExecutingContext context)
+        public void getBanner()
         {
-            ViewBag.AdminName = "" + HttpContext.Session.GetString("AdminName");
+            Banner banner = _context.Banners.OrderBy(x => Guid.NewGuid()).FirstOrDefault();
+            ViewBag.Banner = banner.BannerImg1;
         }
-
-
-        //取得選單
-        public void getMenu()
-        {
-            int GroupNum = Convert.ToInt16(HttpContext.Session.GetString("GroupNum"));
-
-            var module = from c in _context.MenuGroups
-                         where c.MenuGroupPublish == true
-                         orderby c.MenuGroupNum ascending
-                         select c;
-            ViewBag.module = module.ToList();
-
-
-            var moduleFun = from c in _context.MenuSubs
-            join
-                                            s in _context.AdminRoles on c.MenuSubNum equals s.MenuSubNum
-                            where c.MenuSubPublish == true && s.GroupNum == GroupNum
-                            select c;
-
-            ViewBag.moduleFun = moduleFun.ToList();
-        }
-
-        
-        // 登入判斷
-        public bool LoginState()
-        {
-            string AdminNum = "" + HttpContext.Session.GetString("AdminNum");
-            if (AdminNum.Length == 0)
-            {
-                return false;
-            }
-            return true;
-        }
-
-
-        //權限判斷
-        public bool CheckRole(int menuSubNum, string action)
-        {
-            string GroupNum = HttpContext.Session.GetString("GroupNum");
-            if(GroupNum == null)
-            {
-                return false;
-            }
-
-            var role = _context.AdminRoles.Where(x => x.GroupNum == Convert.ToInt16(GroupNum) && x.MenuSubNum == menuSubNum && x.Role.Contains(action));
-            if (!role.Any())
-            {
-                return false;
-            }
-
-            return true;
-        }
-
-
 
         
     }
