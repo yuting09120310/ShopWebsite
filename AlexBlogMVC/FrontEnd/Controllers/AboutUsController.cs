2737806d19fe37385551fde6bfad5fc47056992a
yuting09120310 <yuting09120310@gmail.com>
2023-05-26 14:46:21 +0800

    環境

--- AlexBlogMVC/FrontEnd/Controllers/AboutUsController.cs
+++ AlexBlogMVC/FrontEnd/Controllers/AboutUsController.cs
@@ -13,7 +13,7 @@ namespace AlexBlogMVC.FrontEnd.Controllers
         public IActionResult Index()
         {
             var viewModel = new AboutUsPageViewModel();
-            viewModel.Title = "關於我";
+            viewModel.Title = "關於我 ";
             viewModel.Description = "歡迎來到我的網站！無論您需要一個個人網站、企業網站還是電子商務平台，我都能為您提供量身定制的解決方案。致力於提供優質的網站設計和開發服務。無論您是創業初期還是大型企業，都有適合您的解決方案。讓我們一同打造出色的網站，助您在線世界中取得成功！";
 
             return View(viewModel);
