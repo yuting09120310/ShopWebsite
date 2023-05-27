3ff735a3a9c5f1455ef4f94e220e5fa700f97be9
yuting <yuting09120310@gmail.com>
2023-05-28 01:19:23 +0800

    前台首頁變更

--- AlexBlogMVC/Areas/BackEnd/Models/BlogMvcContext.cs
+++ AlexBlogMVC/Areas/BackEnd/Models/BlogMvcContext.cs
@@ -41,7 +41,7 @@ public partial class BlogMvcContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=127.0.0.1;Database=BlogMVC;User ID=sa;Password=Obiz@0000;Trusted_Connection=True;Integrated Security=False;Encrypt=False;");
+        => optionsBuilder.UseSqlServer("Server=127.0.0.1;Database=BlogMVC;User ID=sa;Password=alex0310;Trusted_Connection=True;Integrated Security=False;Encrypt=False;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
