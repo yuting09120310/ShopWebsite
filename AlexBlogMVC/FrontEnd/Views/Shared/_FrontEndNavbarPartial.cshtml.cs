305b10de80957596055b2a1a9e75be13a9cb7177
yuting <yuting09120310@gmail.com>
2023-05-28 16:45:08 +0800

    拆分完成

---
+++ AlexBlogMVC/FrontEnd/Views/Shared/_FrontEndNavbarPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AlexBlogMVC.FrontEnd.Views.Shared
+{
+    public class _FrontEndNavbarPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
