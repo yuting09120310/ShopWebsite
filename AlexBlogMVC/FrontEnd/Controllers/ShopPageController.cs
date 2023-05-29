bd25afb80b3c7d02091caa0fcb4b881a4e9dcaf3
yuting <yuting09120310@gmail.com>
2023-05-29 22:42:21 +0800

    layout 修改

---
+++ AlexBlogMVC/FrontEnd/Controllers/ShopPageController.cs
@@ -0,0 +1,24 @@
+﻿using AlexBlogMVC.Areas.BackEnd.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace AlexBlogMVC.FrontEnd.Controllers
+{
+    public class ShopPageController : GenericController
+    {
+
+        public ShopPageController(BlogMvcContext context) : base(context) { }
+
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            getNewsType();
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
