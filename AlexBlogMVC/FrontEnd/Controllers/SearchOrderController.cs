d019ef56322ebd36b1dc09f19817288dc37dfcb1
yuting09120310 <yuting09120310@gmail.com>
2023-06-08 12:02:49 +0800

    訂單查詢view

---
+++ AlexBlogMVC/FrontEnd/Controllers/SearchOrderController.cs
@@ -0,0 +1,30 @@
+﻿using AlexBlogMVC.Areas.BackEnd.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace AlexBlogMVC.FrontEnd.Controllers
+{
+    public class SearchOrderController : GenericController
+    {
+
+        public SearchOrderController(BlogMvcContext context) : base(context) { }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            getBanner();
+            getNewsType();
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult SearchResult(string OrderID)
+        {
+            return View();
+        }
+    }
+}
