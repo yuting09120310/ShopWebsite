20e181cd8c66ad02fd2ed61ba83bd9993e6f2895
Alex-PC <yuting09120310@gmail.com>
2023-09-02 02:56:04 +0800

    db修改

--- ShopWebsite/Areas/BackEnd/Models/ShopWebsiteContext.cs
+++ ShopWebsite/Areas/BackEnd/Models/ShopWebsiteContext.cs
@@ -43,7 +43,7 @@ public partial class ShopWebsiteContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.;Database=ShopWebsite;User ID=sa;Password=Obiz@0000;Trusted_Connection=True;Integrated Security=False;Encrypt=False;");
+        => optionsBuilder.UseSqlServer("Server=192.168.0.100;Database=ShopWebsite;User ID=sa;Password=Alex0310;Trusted_Connection=True;Integrated Security=False;Encrypt=False;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
